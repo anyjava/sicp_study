d7a7fa31ac27a08ee18f855e3d695d11340aeaee
Hyuntae Son <anyjava.kr@gmail.com>
2015-09-11 13:04:42 +0900

    책읽으면서 타이핑 실습

--- ch1.clj
+++ ch1.clj
@@ -1,1 +1,12 @@
-(+ 1 1)
+(ns reload.core)
+
+; 17page define square
+(defn square [x] (* x x))
+
+(square 2)
+
+; 18page define sum_of_square
+(defn sum-of-square [x y]
+ 	(+ (square x) (square y)))
+
+(sum-of-square 3 4)
