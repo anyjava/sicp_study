1374a324a119c3c5e97a9de0799c4a009fd937c3
Hyuntae Son <anyjava.kr@gmail.com>
2015-12-10 21:35:22 +0900

    순수함수로만 풀이한 1~10 까지 더하는 방법 in Clojure

---
+++ sum_int.clj
@@ -0,0 +1,22 @@
+; 1 부터 10까지 더하는 예제
+; 순수 함수 4개로 풀었음
+; SICP 1권 75페이지에 나오는 예제입니다.
+
+(ns test)
+(remove-ns 'test)
+(ns test)
+
+(defn sum [term a next b]
+  (if (> a b)
+    0
+    (+ (term a) (sum term (next a) next b))))
+
+(defn inc [n] (+ n 1))
+
+(defn identity [x] x)
+
+(defn sum-integers [a b]
+  (sum identity a inc b))
+
+(sum-integers 1 10)
+
